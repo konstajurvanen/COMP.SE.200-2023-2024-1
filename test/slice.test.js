ffdd76b625969bfb094950cd2ce6dad56a24e5e2
Konsta Jurvanen <konsta.jurvanen@tuni.fi>
2023-12-07 19:21:24 +0200

    Enhance branch coverage and add a test case for slice

--- test/slice.test.js
+++ test/slice.test.js
@@ -25,5 +25,38 @@ describe('slice', () => {
         const array = [1, 2, 3, 4, 5];
         expect(slice(array, 0, -2)).toEqual([1, 2, 3]);
     });
+
+    it('should return a slice of the array when start and end are equal', () => {
+        const array = [1, 2, 3, 4, 5];
+        expect(slice(array, 2, 2)).toEqual([]);
+    });
+
+    it('should return the full array if start and end are not provided', () => {
+        const array = [1, 2, 3, 4, 5];
+        expect(slice(array)).toEqual(array);
+    });
+
+    it('should return a slice of the array when start and end are negative', () => {
+        const array = [1, 2, 3, 4, 5];
+        expect(slice(array, -4, -2)).toEqual([2, 3]);
+    });
+
+    it('should return an empty array when the provided array is null', () => {
+        const array =  null;
+        expect(slice(array, -4, -2)).toEqual([]);
+    });
+
+    it('should return an empty array when the provided array is empty', () => {
+        const array =  [];
+        expect(slice(array, -5, 2)).toEqual([]);
+    });
+
+    it('should return a slice from the beginning of array if start is defined to be more than the array length from the end', () => {
+        const array =  [1, 2];
+        expect(slice(array, -3)).toEqual([1, 2]);
+    });
+
+
+
 });
 
